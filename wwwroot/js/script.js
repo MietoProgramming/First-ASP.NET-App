40928294431a5ee1193d4fd3668a18c8fc5b08e4
MietoProgramming <kamdox7@gmail.com>
2020-04-05 00:33:33 +0200

    Finished basic template of web page. - Header - Footer - Side panel

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -2,3 +2,25 @@ function clearSearch(el)
 {
     el.value = "";
 }
+
+function Toggle(text) {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-left",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "500",
+        "hideDuration": "2000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "slideDown",
+        "hideMethod": "slideUp",
+        "escapeHtml": true
+    };
+    toastr.warning(text.toString() + " doesn\'t work currently :(", "Warning!");
+}
